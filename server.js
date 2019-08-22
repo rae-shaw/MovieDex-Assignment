cde97e172fd1f90d308eb8d93ef8742b401e0b5e
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-08-22 15:30:11 -0500

    ready for production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const app = express()
 const movieList = require('./movies-data-small.json');
 
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -19,6 +20,17 @@ app.use(function validateBearerToken(req, res, next){
 	}
 	next()
 })
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 
 
 app.get('/movie', function handleGetMovie(req, res){
@@ -48,7 +60,7 @@ app.get('/movie', function handleGetMovie(req, res){
 })
 
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
